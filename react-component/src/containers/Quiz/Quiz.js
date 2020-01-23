ba7aa22ba9b4ebc5ceadb6a4d8329e26504b88e4
Garza0 <konovalovandrey7@gmail.com>
2020-01-23 23:23:50 +0200

    quiz finished log

--- react-component/src/containers/Quiz/Quiz.js
+++ react-component/src/containers/Quiz/Quiz.js
@@ -35,9 +35,30 @@ class Quiz extends Component {
   onAnswerClickHandler = (answerId) => {
     console.log('Answer Id: ', answerId)
 
-    this.setState({
-      activeQuestion: this.state.activeQuestion + 1
-    })
+    const question = this.state.quiz[this.state.activeQuestion]
+
+    if (question.rightAnswerId === answerId) {
+
+      const timeout = window.setTimeout(() => {
+        if (this.isQuizFinished) {
+          console.log('finished')
+        } else {
+          this.setState({
+            activeQuestion: this.state.activeQuestion + 1
+          })
+        }
+        window.clearTimeout(timeout)
+      }, 1000)
+
+    } else {
+
+    }
+
+  }
+
+
+  isQuizFinished() {
+    return this.state.activeQuestion + 1 === this.state.quiz.length
   }
 
   render() {
