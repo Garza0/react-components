8bc1d8e9dcb1f93c15e538980b11fb996dbb691a
Garza0 <konovalovandrey7@gmail.com>
2020-01-29 23:23:53 +0200

    redux1

--- react-component/src/index.js
+++ react-component/src/index.js
@@ -4,10 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom'
-import { createStore } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import rootReducer from './store/reducers.rootReducer'
+import thunk from 'redux-thunk'
 
-const store = createStore()
+const composeEnhancers =
+  typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    }) : compose;
+
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(thunk)
+  ))
 
 
 
